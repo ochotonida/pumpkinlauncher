f0b795beeaff25659ca08fe330710583a44a2060
Wouter <wouter@grijsloke.be>
2018-10-04 22:34:45 +0200

    Changed item usage

--- src/main/java/pumpkinlauncher/item/ItemPumpkinLauncher.java
+++ src/main/java/pumpkinlauncher/item/ItemPumpkinLauncher.java
@@ -1,11 +1,12 @@
 package pumpkinlauncher.item;
 
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.SoundEvents;
+import net.minecraft.item.EnumAction;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumHand;
@@ -23,36 +24,38 @@ public class ItemPumpkinLauncher extends Item {
         setUnlocalizedName("pumpkinlauncher");
         setCreativeTab(CreativeTabs.COMBAT);
         setMaxStackSize(1);
-        this.setMaxDamage(0);
     }
 
     @Nonnull
     @Override
     @ParametersAreNonnullByDefault
     public ActionResult<ItemStack> onItemRightClick(World world, EntityPlayer player, EnumHand hand) {
-        ItemStack stack = player.getHeldItem(hand);
-        NBTTagCompound nbtTagCompound = stack.getTagCompound();
+        player.setActiveHand(hand);
+        return new ActionResult<>(EnumActionResult.SUCCESS, player.getHeldItem(hand));
+    }
 
-        if (nbtTagCompound == null) {
-            nbtTagCompound = new NBTTagCompound();
-            nbtTagCompound.setBoolean("Loaded", false);
-            stack.setTagCompound(nbtTagCompound);
+    @Nonnull
+    @Override
+    @ParametersAreNonnullByDefault
+    public ItemStack onItemUseFinish(ItemStack stack, World world, EntityLivingBase entity) {
+        world.playSound(null, entity.posX, entity.posY, entity.posZ, SoundEvents.ENTITY_ITEM_BREAK, SoundCategory.NEUTRAL, 0.8F, 1.6F);
+
+        if (!world.isRemote) {
+            EntityPumkinProjectile projectile = new EntityPumkinProjectile(world, entity);
+            projectile.shoot(entity, entity.rotationPitch, entity.rotationYaw, 1.1F, 2.5F);
+            world.spawnEntity(projectile);
         }
+        return super.onItemUseFinish(stack, world, entity);
+    }
 
-        if (nbtTagCompound.getBoolean("Loaded")) {
-            nbtTagCompound.setBoolean("Loaded", false);
-            world.playSound(null, player.posX, player.posY, player.posZ, SoundEvents.ENTITY_ITEM_BREAK, SoundCategory.NEUTRAL, 0.8F, 0.3F);
+    @Override
+    public int getMaxItemUseDuration(ItemStack stack) {
+        return 20;
+    }
 
-            if (!world.isRemote) {
-                EntityPumkinProjectile projectile = new EntityPumkinProjectile(world, player);
-                projectile.shoot(player, player.rotationPitch, player.rotationYaw, 1.1F, 2.5F);
-                world.spawnEntity(projectile);
-            }
-            return new ActionResult<>(EnumActionResult.SUCCESS, stack);
-        } else {
-            nbtTagCompound.setBoolean("Loaded", true);
-            world.playSound(null, player.posX, player.posY, player.posZ, SoundEvents.ENTITY_ITEM_BREAK, SoundCategory.NEUTRAL, 0.8F, 2.0F);
-            return new ActionResult<>(EnumActionResult.SUCCESS, stack);
-        }
+    @Nonnull
+    @Override
+    public EnumAction getItemUseAction(ItemStack stack) {
+        return EnumAction.BOW;
     }
 }
