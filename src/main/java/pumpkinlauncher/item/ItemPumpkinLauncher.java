10db0c54a7484bd22e3effccbc08d50d40966673
Wouter <wouter@grijsloke.be>
2018-10-05 23:23:40 +0200

    Now uses built-in cooldown tracker

--- src/main/java/pumpkinlauncher/item/ItemPumpkinLauncher.java
+++ src/main/java/pumpkinlauncher/item/ItemPumpkinLauncher.java
@@ -1,12 +1,9 @@
 package pumpkinlauncher.item;
 
 import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.SoundEvents;
-import net.minecraft.item.EnumAction;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
+import net.minecraft.item.*;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumHand;
@@ -30,32 +27,18 @@ public class ItemPumpkinLauncher extends Item {
     @Override
     @ParametersAreNonnullByDefault
     public ActionResult<ItemStack> onItemRightClick(World world, EntityPlayer player, EnumHand hand) {
-        player.setActiveHand(hand);
+        if (!world.isRemote) {
+            world.playSound(null, player.posX, player.posY, player.posZ, SoundEvents.ENTITY_FIREWORK_BLAST, SoundCategory.NEUTRAL, 1.0F, 0.6F);
+            player.getCooldownTracker().setCooldown(this, 30);
+            EntityPumkinProjectile projectile = new EntityPumkinProjectile(world, player);
+            projectile.shoot(player, player.rotationPitch, player.rotationYaw, 1.3F, 5F);
+            world.spawnEntity(projectile);
+        }
         return new ActionResult<>(EnumActionResult.SUCCESS, player.getHeldItem(hand));
     }
 
-    @Nonnull
     @Override
-    @ParametersAreNonnullByDefault
-    public ItemStack onItemUseFinish(ItemStack stack, World world, EntityLivingBase entity) {
-        world.playSound(null, entity.posX, entity.posY, entity.posZ, SoundEvents.ENTITY_ITEM_BREAK, SoundCategory.NEUTRAL, 0.8F, 1.6F);
-
-        if (!world.isRemote) {
-            EntityPumkinProjectile projectile = new EntityPumkinProjectile(world, entity);
-            projectile.shoot(entity, entity.rotationPitch, entity.rotationYaw, 1.1F, 2.5F);
-            world.spawnEntity(projectile);
-        }
-        return super.onItemUseFinish(stack, world, entity);
-    }
-
-    @Override
-    public int getMaxItemUseDuration(ItemStack stack) {
-        return 20;
-    }
-
-    @Nonnull
-    @Override
-    public EnumAction getItemUseAction(ItemStack stack) {
-        return EnumAction.BOW;
+    public boolean isFull3D() {
+        return true;
     }
 }
