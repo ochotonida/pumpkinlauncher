5554afec2fa9184d86cf003bd34714990aeef385
Wouter <wouter@grijsloke.be>
2018-10-25 12:18:17 +0200

    Updated JEIPlugin

--- src/main/java/pumpkinlauncher/integration/JEIPlugin.java
+++ src/main/java/pumpkinlauncher/integration/JEIPlugin.java
@@ -27,12 +27,13 @@ public class JEIPlugin implements IModPlugin {
     private void addContainerRecipes(IModRegistry registry) {
         NBTTagCompound compound = new NBTTagCompound();
         compound.setByte("power", (byte) 1);
-        NBTTagCompound compound0 = compound.copy();
+        NBTTagCompound compound0 = new NBTTagCompound();
+        compound0.setByte("power", (byte) 3);
         NBTTagCompound compound1 = compound.copy();
         compound1.setBoolean("isFiery", true);
         NBTTagCompound compound2 = compound.copy();
         compound2.setBoolean("canDestroyBlocks", false);
-        NBTTagCompound compound3 = compound.copy();
+        NBTTagCompound compound3 = new NBTTagCompound();
         compound3.setByte("bounceAmount", (byte) 1);
 
         NBTTagCompound fireworkCompound = new NBTTagCompound();
@@ -47,8 +48,6 @@ public class JEIPlugin implements IModPlugin {
 
         NBTTagCompound compound4 = new NBTTagCompound();
         compound4.setTag("fireworks", fireworkCompound.copy());
-        NBTTagCompound compound5 = compound.copy();
-        compound5.setTag("fireworks", fireworkCompound.copy());
         NBTTagCompound compound6 = new NBTTagCompound();
         compound6.setTag("potionTag", new ItemStack(Items.SPLASH_POTION).writeToNBT(new NBTTagCompound()));
         NBTTagCompound compound7 = new NBTTagCompound();
@@ -76,8 +75,6 @@ public class JEIPlugin implements IModPlugin {
         output3.setTagCompound(compound3);
         ItemStack output4 = new ItemStack(PumpkinLauncher.PUMPKIN_AMMO, 3);
         output4.setTagCompound(compound4);
-        ItemStack output5 = new ItemStack(PumpkinLauncher.PUMPKIN_AMMO, 3);
-        output5.setTagCompound(compound5);
         ItemStack output6 = new ItemStack(PumpkinLauncher.PUMPKIN_AMMO, 3);
         output6.setTagCompound(compound6);
         ItemStack output7 = new ItemStack(PumpkinLauncher.PUMPKIN_AMMO, 3);
@@ -104,12 +101,11 @@ public class JEIPlugin implements IModPlugin {
         firework.setTagCompound(fireworkItemCompound);
 
         registry.addRecipes(Arrays.asList(
-                new ItemStackRecipeWrapper(output0, pumpkin, gunpowder),
+                new ItemStackRecipeWrapper(output0, pumpkin, gunpowder, gunpowder, gunpowder),
                 new ItemStackRecipeWrapper(output1, pumpkin, gunpowder, new ItemStack(Items.FIRE_CHARGE, 1, OreDictionary.WILDCARD_VALUE)),
                 new ItemStackRecipeWrapper(output2, pumpkin, gunpowder, new ItemStack(Blocks.WOOL, 1, OreDictionary.WILDCARD_VALUE)),
-                new ItemStackRecipeWrapper(output3, pumpkin, gunpowder, new ItemStack(Items.SLIME_BALL, 1, OreDictionary.WILDCARD_VALUE)),
+                new ItemStackRecipeWrapper(output3, pumpkin, new ItemStack(Items.SLIME_BALL, 1, OreDictionary.WILDCARD_VALUE)),
                 new ItemStackRecipeWrapper(output4, pumpkin, firework),
-                new ItemStackRecipeWrapper(output5, pumpkin, gunpowder, firework),
                 new ItemStackRecipeWrapper(output6, pumpkin, new ItemStack(Items.SPLASH_POTION, 1, OreDictionary.WILDCARD_VALUE)),
                 new ItemStackRecipeWrapper(output7, pumpkin, new ItemStack(Items.LINGERING_POTION, 1, OreDictionary.WILDCARD_VALUE)),
                 new ItemStackRecipeWrapper(output8, pumpkin, new ItemStack(Blocks.BONE_BLOCK, 1, OreDictionary.WILDCARD_VALUE)),
