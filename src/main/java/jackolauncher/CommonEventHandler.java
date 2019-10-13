951107b0277c740f62bd21cc44272a6563120f57
Wouter <wouter@grijsloke.be>
2019-10-13 09:39:07 +0200

    Fixed a potential compatibility issue

--- src/main/java/jackolauncher/CommonEventHandler.java
+++ src/main/java/jackolauncher/CommonEventHandler.java
@@ -18,11 +18,11 @@ public class CommonEventHandler {
                 || event.getName().equals(new ResourceLocation("minecraft:chests/buried_treasure"))
                 || event.getName().equals(new ResourceLocation("minecraft:chests/jungle_temple_dispenser"))) {
             LootEntry.Builder entry = TableLootEntry.builder(new ResourceLocation(JackOLauncher.MODID, "inject/jack_o_ammo")).weight(1);
-            LootPool pool = LootPool.builder().addEntry(entry).build();
+            LootPool pool = LootPool.builder().addEntry(entry).name("jack_o_ammo_injection").build();
             event.getTable().addPool(pool);
         } else if (event.getName().equals(new ResourceLocation("minecraft:chests/village/village_weaponsmith"))) {
             LootEntry.Builder entry = TableLootEntry.builder(new ResourceLocation(JackOLauncher.MODID, "inject/jack_o_launcher")).weight(1);
-            LootPool pool = LootPool.builder().addEntry(entry).build();
+            LootPool pool = LootPool.builder().addEntry(entry).name("jack_o_launcher_injection").build();
             event.getTable().addPool(pool);
         }
     }
